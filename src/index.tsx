81d83834b481b4cab1e8be351fe71c05ba4a1736
Joao Vitor Freitas <jvfreitas02@gmail.com>
2022-08-09 01:34:35 -0300

    Iniciando o banco vazio e limpando importações

--- src/index.tsx
+++ src/index.tsx
@@ -10,40 +10,7 @@ createServer({
 
   seeds(server) {
     server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Desenvolvimento App',
-          amount: 5000.0,
-          type: 'deposit',
-          category: 'Job',
-          createdAt: new Date('2020-01-01 09:00:00'),
-        },
-        {
-          id: 2,
-          title: 'Desenvolvimento Website',
-          amount: 5000.0,
-          type: 'deposit',
-          category: 'Job',
-          createdAt: new Date('2020-02-02 09:00:00'),
-        },
-        {
-          id: 3,
-          title: 'Razer Viper Ultimate',
-          amount: 900,
-          type: 'withdraw',
-          category: 'Periferics',
-          createdAt: new Date('2020-01-10 09:00:00'),
-        },
-        {
-          id: 4,
-          title: 'Razer Huntsman Mini',
-          amount: 699,
-          type: 'withdraw',
-          category: 'Periferics',
-          createdAt: new Date('2020-01-10 09:00:00'),
-        },
-      ],
+      transactions: [],
     });
   },
 
